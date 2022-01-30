4f154433510999edb2413fac215ee860093cae4e
Víctor Morilla <vmorilla@gmail.com>
2022-01-30 20:24:08 +0100

    Authenticación en página separada

---
+++ static/js/auth-config.js
@@ -0,0 +1,32 @@
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: "https://app.jazzforge.com",
+    signInOptions: [
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            clientId: CLIENT_ID
+        
+        },
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+            requireDisplayName: true // Does not work...
+        },
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
