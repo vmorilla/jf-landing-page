f5cd830aa476396feedec44a03c2e1f76a583b34
Víctor Morilla <vmorilla@gmail.com>
2022-02-06 16:54:48 +0100

    predeploy con signin react app

--- predeploy.sh
+++ predeploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 (cd ../packages/changes && flutter build web --web-renderer canvaskit)
-(cp -R ../packages/changes/build/web static/app)
+cp -R ../packages/changes/build/web static/app
+(cd ../login && npm install && npm run build)
+cp -R ../login/build/static/js/main.*.js static/js/signin.js
+cp -R ../login/build/static/css/main.*.css static/css/signin.css
+
 hugo
