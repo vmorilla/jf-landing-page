652da2a6f50fe195145a9fc2096fae1dbcc17bcc
Víctor Morilla <vmorilla@gmail.com>
2022-02-28 00:07:41 +0100

    Corrección scripts despliegue

--- predeploy-flutter.sh
+++ predeploy-flutter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 (cd ../packages/changes && flutter build web --web-renderer canvaskit)
-cp -R ../packages/changes/build/web static/app
+rm -fR static/app
+cp -R ../packages/changes/build/web/ static/app
