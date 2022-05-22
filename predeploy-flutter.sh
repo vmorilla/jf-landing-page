1de8d50ff58eb746e8b546a1810ea7ce7515b914
Víctor Morilla <vmorilla@gmail.com>
2022-05-22 17:18:47 +0200

    BaseHref en el deploy

--- predeploy-flutter.sh
+++ predeploy-flutter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-(cd ../packages/changes && flutter build web --web-renderer canvaskit)
+(cd ../packages/changes && flutter build web --web-renderer canvaskit --base-href '/app/')
 rm -fR static/app
 cp -R ../packages/changes/build/web/ static/app
